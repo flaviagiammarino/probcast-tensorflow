e8dad61878cd9a8a46d5f8c3a0758980342a29bc
flaviagiammarino <flavia.giammarino@gmail.com>
2022-11-27 07:02:34 +0100

    reorganize code

--- probcast_tensorflow/losses.py
+++ probcast_tensorflow/losses.py
@@ -8,7 +8,7 @@ def generator_loss(targets, predictions, prob_predictions):
     L1 = tf.keras.losses.MeanAbsoluteError()
     L2 = tf.keras.losses.BinaryCrossentropy()
     
-    return 0.99 * L1(y_true=targets, y_pred=predictions) + 0.01 * L2(y_true=tf.ones_like(prob_predictions), y_pred=prob_predictions)
+    return L1(y_true=targets, y_pred=predictions) + L2(y_true=tf.ones_like(prob_predictions), y_pred=prob_predictions)
 
 
 def discriminator_loss(prob_targets, prob_predictions):
